25d496d970fa1ef1dd984cbb1cbb647f4a1f7271
faridaahmed214 <faridaahmed212004@gmail.com>
2025-12-28 17:49:48 +0200

    Add SwapByRef

---
+++ OOP/SwapByRef/README.md
@@ -0,0 +1,7 @@
+# SwapByRef
+
+Description: Demonstrates swapping values by reference in C++.
+
+Main files: `main.cpp`.
+
+Concepts: references, pointers, function arguments.
---
+++ OOP/SwapByRef/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &x, int &y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+int main() {
+    int a, b;
+
+    cout << "enter first number: ";
+    cin >> a;
+    cout << "enter second number: ";
+    cin >> b;
+
+    cout << "\nbefore : a = " << a << ", b = " << b << endl;
+
+    swap(a, b);
+
+    cout << "after :  a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
