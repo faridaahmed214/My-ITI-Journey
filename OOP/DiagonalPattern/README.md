6f00c36ec072d8a296e2152e46677a4beb4b128a
faridaahmed214 <faridaahmed212004@gmail.com>
2025-12-28 17:49:41 +0200

    Add DiagonalPattern

---
+++ OOP/DiagonalPattern/README.md
@@ -0,0 +1,7 @@
+# DiagonalPattern
+
+Description: Prints diagonal/star patterns in C++ console.
+
+Main files: `main.cpp`.
+
+Concepts: nested loops, formatting output.
---
+++ OOP/DiagonalPattern/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (i == j)
+                cout << " * ";
+            else
+                cout << " - ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
